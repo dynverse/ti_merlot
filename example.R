26200baa5e7f45e6c9c5c37e30a87be511d7dfc1
zouter <wouter.saelens@gmail.com>
2018-09-04 20:13:29 +0200

    update example

--- example.R
+++ example.R
@@ -1,10 +1,10 @@
 set.seed(1)
 data <- dyntoy::generate_dataset(
   id = "specific_example/merlot",
-  num_cells = 99,
-  num_features = 101,
+  num_cells = 500,
+  num_features = 200,
   model = "tree"
 )
 params <- list(
-  N_yk = 25
+  N_yk = 1
 )
